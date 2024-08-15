58feb50a5db37b1d17b6fc5918d740cae99e8c34
tejas.soni <tejassoni@york.ie>
2024-08-15 09:56:56 +0530

    check log

--- controllers/handleSlackOauthRedirect.js
+++ controllers/handleSlackOauthRedirect.js
@@ -3,6 +3,14 @@ const { slackApp } = require(".");
 /* Search messages globally */
 const handleSlackOauthRedirect = async (req, res) => {
   try {
+    console.log(
+      "handleSlackOauthRedirect  process.env.SLACK_CLIENT_ID:",
+      process.env.SLACK_CLIENT_ID
+    );
+    console.log(
+      "handleSlackOauthRedirect  process.env.SLACK_CLIENT_SECRET:",
+      process.env.SLACK_CLIENT_SECRET
+    );
     const { code } = req.query;
     const result = await slackApp.client.openid.connect.token({
       client_id: process.env.SLACK_CLIENT_ID,
@@ -10,6 +18,7 @@ const handleSlackOauthRedirect = async (req, res) => {
       redirect_uri:
         "https://winrate-slack-demo.onrender.com/slack-oauth-redirect",
       client_secret: process.env.SLACK_CLIENT_SECRET,
+      grant_type: 'authorization_code',
     });
     return res.status(200).json({
       message: "Successfully handled oauth...",
